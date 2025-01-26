50e72de8a46eb274f429bffe98d8f0f9ac3a04c2
Mihirxp <palkarmihir21@gmail.com>
2025-01-26 10:18:19 +0530

    Java code

---
+++ balance.java
@@ -0,0 +1,44 @@
+public class Account {
+    private int accountId;
+    private String username;
+    private String password;
+    private double balance;
+
+    public Account(String username, String password, double balance) {
+        this.username = username;
+        this.password = password;
+        this.balance = balance;
+    }
+
+    public int getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(int accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
